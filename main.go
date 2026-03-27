cc8147f1dffb788c192b42b2f8460a76db089075
Duy Nguyen <emailcuaban@gmail.com>
2026-03-27 20:15:46 +0700

    test branch

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"go-student-manager/handlers"
 	"go-student-manager/services"
 )
 
 func main() {
+	fmt.Println("CHO AN CUT")
 	services.ShowStudent()
 	handlers.Menu()
 	services.Addstudent()
